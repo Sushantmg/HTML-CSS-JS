c3ba9ad9911b9d87b25062c9c82a13a621c61f3c
Sushantmg <sushantmg2004@gmail.com>
2025-04-03 15:45:43 +0545

    variableChange

--- file_js/variable.js
+++ file_js/variable.js
@@ -2,7 +2,9 @@
 let userName = "John";
 let age = 20;
 let isStudent = true;
+let gender = "Male";
 
 console.log("Name:", userName);
 console.log("Age:", age);
 console.log("Is a Student:", isStudent);
+console.log("Gender is :", gender);
